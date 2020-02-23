d5bffa0fbd5ba47b8c5fd48960ddf0cee3ccba88
Shubham Singh Chahar <thefallenmerc@gmail.com>
2020-02-23 23:09:45 +0530

    Updates

---
+++ precache-manifest.015794599c3a2a96d25e37d47a614037.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77466f04d3d57cb5c8625730689e66e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c23289d48f30fba8987",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "b567213d37af7adeaf49",
+    "url": "/static/css/main.a570f9df.chunk.css"
+  },
+  {
+    "revision": "7c23289d48f30fba8987",
+    "url": "/static/js/2.8e735e22.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.8e735e22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b567213d37af7adeaf49",
+    "url": "/static/js/main.fe8e1a56.chunk.js"
+  },
+  {
+    "revision": "42a8b147aa10dbde66ec",
+    "url": "/static/js/runtime-main.9f7c15aa.js"
+  }
+]);
