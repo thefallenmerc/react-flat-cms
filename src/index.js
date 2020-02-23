5e9c3f95deda5eec447ad0c7b3e3481d25df0bb9
Shubham Singh Chahar <thefallenmerc@gmail.com>
2020-02-23 23:12:20 +0530

    added github io routing support

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { render } from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 import App from './App';
 
-render(<BrowserRouter><App /></BrowserRouter>, document.getElementById("root"));
+if(window.location.pathname.indexOf("github.io") >= 0) {
+    render(<HashRouter><App /></HashRouter>, document.getElementById("root"));
+} else {
+    render(<BrowserRouter><App /></BrowserRouter>, document.getElementById("root"));
+}
