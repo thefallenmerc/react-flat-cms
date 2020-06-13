978a4024aef536b5ede36ad48af7e2ae0ccd06ce
Shubham Singh Chahar <thefallenmerc@gmail.com>
2020-06-13 16:39:18 +0530

    1. Removed extra pages 2. Added hot:reload on file change

--- src/MDs/javascript/core/hello_world.md
+++
@@ -1,7 +0,0 @@
-### How to create Hello World in Javascript
-
-1. Use the following code on browser console - 
-
-```javascript
-    console.log('Hello World');
-```
--- src/Router.js
+++ src/Router.js
@@ -3,16 +3,12 @@ import React from 'react';
 
 import FourOFour from './components/FourOFour';
 
-import JavascriptCoreHelloWorld from "./pages/javascript/core/hello_world.jsx";
-import PhpCoreHelloWorld from "./pages/php/core/hello_world.jsx";
 import ReactFlatReadme from "./pages/ReactFlat/readme.jsx";
 
 export default class Router extends React.Component {
     render() {
         return (
             <Switch>
-				<Route path={"/javascript/core/hello-world"} component={JavascriptCoreHelloWorld} />
-				<Route path={"/php/core/hello-world"} component={PhpCoreHelloWorld} />
 				<Route path={"/reactflat/readme"} component={ReactFlatReadme} />
                 <Route path="/:page" render={props => <FourOFour />} />
             </Switch>
